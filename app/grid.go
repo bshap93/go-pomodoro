fb3e4c73e2170a897ab13411f359b722679165c7
Ben Shapiro <bshap93@gmail.com>
2023-04-12 18:29:29 -0600

    Any

--- app/grid.go
+++ app/grid.go
@@ -1,17 +1,74 @@
 package app
 
-
 import (
-  "github.com/mum4k/termdash/align" 
-  "github.com/mum4k/termdash/container" 
-  "github.com/mum4k/termdash/container/grid" 
-  "github.com/mum4k/termdash/linestyle" 
-  "github.com/mum4k/termdash/terminal/terminalapi"
+	"github.com/mum4k/termdash/align"
+	"github.com/mum4k/termdash/container"
+	"github.com/mum4k/termdash/container/grid"
+	"github.com/mum4k/termdash/linestyle"
+	"github.com/mum4k/termdash/terminal/terminalapi"
 )
 
-func newGrid(b *buttonSet, w *widgets,k
-  t terminalapi.Terminal) (*container.Container, error) {
+func newGrid(b *buttonSet, w *widgets,
+	t terminalapi.Terminal) (*container.Container, error) {
 
-  builder := grid.New()
+	builder := grid.New()
 
+	builder.Add(
+		grid.RowHeightPerc(30,
+			grod.ColWidthPerc(30,
+				[]container.Option{
+					container.Border(linestyle.Light),
+					container.BorderTitle("Press Q to Quit"),
+				}, // container options
+				grid.RowHeightPerc(80,
+					grid.Widget(w.donTimer)),
+				grid.RowHeightPerc(20,
+					[]container.Option{
+						container.AlignHorizontal(align.HorizontalCenter),
+					}, // container options
+					grid.Widget(w.txtTimer,
+						container.AlignHorizontal(align.HorizontalCenter),
+						container.AlignVertical(align.VerticalMiddle),
+						container.PaddingLeftPercent(49),
+					), // widget options
+				), // row options
+			), // col options
+			grid.ColWidthPerc(70,
+				grid.RowHeightPerc(80,
+					grid.Widget(w.disType, container.Border(linestyle.Light)),
+				), // row options
+				grid.RowHeightPerc(20,
+					grid.Widget(w.txtInfo, container.Border(linestyle.Light)),
+				), // row options
+			), // col options
+		), // row options
+	) // builder.Add
 
+	builder.Add(
+		grid.RowHeightPerc(10,
+			grid.ColWidthPerc(50,
+				grid.Widget(b.btStart),
+			), // col options
+			grid.ColWidthPerc(50,
+				grid.Widget(b.btPause),
+			), // col options
+		), // row options
+	) // builder.Add
+
+	// Add third row
+	builder.Add(
+		grid.RowHeightPerc(60),
+	)
+
+	gridOpts, err := builder.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := container.New(t, gridOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
